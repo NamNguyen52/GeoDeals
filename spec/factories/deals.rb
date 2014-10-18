a3ff5768d26c569137d97fe01cc4fb403e72b3b2
NamNguyen52 <nam.nguyen52@gmail.com>
2014-10-17 19:24:34 -0700

    added models and defined relationships

---
+++ spec/factories/deals.rb
@@ -0,0 +1,15 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :deal do
+    name "MyString"
+    description "MyString"
+    code "MyString"
+    fine "MyString"
+    start_date "2014-10-17"
+    end_date "2014-10-17"
+    start_time "2014-10-17 18:37:59"
+    end_time "2014-10-17 18:37:59"
+    business_id "MyString"
+  end
+end
