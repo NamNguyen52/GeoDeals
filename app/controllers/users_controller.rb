886c8ad70fc282422800462e4a15bb87d45b101b
msmilansky <msmilansky@gmail.com>
2014-10-23 20:24:51 -0700

    fixed redirects upon user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,13 @@ respond_to :json, :html
   def create
     @user = User.new(params.require(:user).permit(:first_name, :last_name, :email, :email_confirmation, :password, :password_confirmation, :business_owner, :customer))
 	  if @user.save
+      if @user.business_owner == true
       log_in(@user)
-      redirect_to user_path(@user) 
+      redirect_to new_business_path
+    elsif @user.save
+      if @user.business_owner == false
+        log_in(@user)
+        redirect_to sessions_path
     elsif @user.errors.messages[:password] && @user.errors.messages[:email] 
 	    @user.email = ""
 	    @user.email_confirmation = ""
@@ -33,6 +38,7 @@ respond_to :json, :html
 	   render 'new'
     end
   end
+end
 
   def show
     @user = User.find(params[:id])
@@ -45,4 +51,5 @@ respond_to :json, :html
       redirect_to sessions_path(@user)
     end
   end
+end
 end
