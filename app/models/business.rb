f0f4d9b6ada2e9adec0bac2f4ee2fe2c902f3d9e
msmilansky <msmilansky@gmail.com>
2014-10-27 18:44:18 -0700

    business index styling, nested attributes in business settings, started rspec tests for user model

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,8 @@
 class Business < ActiveRecord::Base
 	has_many :deals
-	has_many :users, as: :admins
+	has_one :user
+
+	accepts_nested_attributes_for :user, :deals
+
+	attr_accessor :business, :user_attributes
 end
