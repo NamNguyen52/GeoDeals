4eff43a7782d3e02674d4d1637cec01678ded134
NamNguyen52 <nam.nguyen52@gmail.com>
2014-10-29 17:08:37 -0700

    added more styling to homepage

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -22,8 +22,12 @@ include SessionsHelper
   def create
     @deal = Deal.new(params.require(:deal).permit(:name, :description, :fine, :start_date, :end_date, :start_time, :end_time))
       @deal.business_id = current_user.business_id
-      @deal.latitude = current_user.latitude
-      @deal.longtitude = current_user.longtitude
+      curr_user_biz_id = current_user.business_id
+      curr_user_biz_obj = Business.find_by_id(curr_user_biz_id)
+      curr_user_biz_lng = curr_user_biz_obj.longitude
+      curr_user_biz_lat = curr_user_biz_obj.latitude
+      @deal.lat = curr_user_biz_lat
+      @deal.lng = curr_user_biz_lng
       if @deal.save
         redirect_to business_index_path
       else
